f3b92500c195ba4f02ddc37f1d7692238c5c019b
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-10 16:43:06 +0100

    add html structure and add distance and age const

---
+++ main.js
@@ -0,0 +1,3 @@
+// Chiedi all'utente il numero di chilometri e l'età del passeggero
+const distance = parseFloat(prompt("Quanta distanza vuoi fare in kilometri? esempio:[15]"));
+const age = parseInt(prompt("Quanti anni hai? esempio:[25]"));

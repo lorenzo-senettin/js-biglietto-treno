4674187af645bbf188eb8e2f2ae3ea96171bfbb4
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-10 17:03:28 +0100

    chore: fix minor space typo

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ const distance = parseFloat(prompt("Quanta distanza vuoi fare in kilometri? esem
 const age = parseInt(prompt("Quanti anni hai? esempio:[25]"));
 
 
-// Costo del biglietto per ogni kilometro [espresso in euro]
+// Costo del biglietto per ogni chilometro [espresso in euro]
 const price_distance = 0.21;
 
 
-//Calcola il prezzo del biglietto in base ai chilometri
+// Calcola il prezzo del biglietto in base ai chilometri
 let price = distance * price_distance;
 
 
-//Calcola il prezzo del biglietto con variazioni di prezzo
-if (age < 18) { //Prezzo per persone con meno di 18 anni
+// Calcola il prezzo del biglietto con variazioni di prezzo
+if (age < 18) { // Prezzo per persone con meno di 18 anni
   price *= 0.8;
 }
-if (age > 65) { //Prezzo per persone con più di 65 anni
+if (age > 65) { // Prezzo per persone con più di 65 anni
   price *= 0.6;
 }
 
 
-//Calcolare il prezzo finale e stamparlo a schermo
+// Calcolare il prezzo finale e stamparlo a schermo
 const final_price = price.toFixed(2);
 console.log(final_price);

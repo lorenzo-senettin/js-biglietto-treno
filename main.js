f9c9c28f6c696e2f464110cbc67450db6517e5cf
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-10 16:48:14 +0100

    add ticket price per km and final price

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // Chiedi all'utente il numero di chilometri e l'età del passeggero
 const distance = parseFloat(prompt("Quanta distanza vuoi fare in kilometri? esempio:[15]"));
 const age = parseInt(prompt("Quanti anni hai? esempio:[25]"));
+
+// Costo standard del biglietto per ogni kilometro [espresso in euro]
+const price_distance = 0.21;
+
+//Calcola il prezzo del biglietto in base ai chilometri
+let price = distance * price_distance;
